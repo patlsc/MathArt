34f24cf207464dda944a6a498e8b60a215a13957
patlsc <plschmittt@gmail.com>
2022-09-30 01:14:45 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -18,6 +18,8 @@ const int NUM_STEPS = 500;
 
 const int NUM_LINES = 900;
 
+const char OUTPUT_PATH[] = "out3.bmp";
+
 int32_t XCordToPixel(double xPos) {
     return (int32_t)((xPos - MIN_X)* WINDOW_W / (MAX_X - MIN_X));
 }
@@ -110,25 +112,60 @@ void DrawCircle(SDL_Renderer *renderer, int32_t centreX, int32_t centreY, int32_
     }
 }
 
+bool SaveScreenshotBMP(SDL_Window* SDLWindow, SDL_Renderer* SDLRenderer) {
+    SDL_Surface* saveSurface = NULL;
+    SDL_Surface* infoSurface = NULL;
+    infoSurface = SDL_GetWindowSurface(SDLWindow);
+    if (infoSurface == NULL) {
+
+    }
+    else {
+        unsigned char* pixels = new (std::nothrow) unsigned char[infoSurface->w * infoSurface->h * infoSurface->format->BytesPerPixel];
+        if (pixels == 0) {
+            return false;
+        }
+        else {
+            if (SDL_RenderReadPixels(SDLRenderer, &infoSurface->clip_rect, infoSurface->format->format, pixels, infoSurface->w * infoSurface->format->BytesPerPixel) != 0) {
+                delete[] pixels;
+                return false;
+            }
+            else {
+                saveSurface = SDL_CreateRGBSurfaceFrom(pixels, infoSurface->w, infoSurface->h, infoSurface->format->BitsPerPixel, infoSurface->w * infoSurface->format->BytesPerPixel, infoSurface->format->Rmask, infoSurface->format->Gmask, infoSurface->format->Bmask, infoSurface->format->Amask);
+                if (saveSurface == NULL) {
+                    delete[] pixels;
+                    return false;
+                }
+                SDL_SaveBMP(saveSurface, OUTPUT_PATH);
+                SDL_FreeSurface(saveSurface);
+                saveSurface = NULL;
+            }
+            delete[] pixels;
+        }
+        SDL_FreeSurface(infoSurface);
+        infoSurface = NULL;
+    }
+    return true;
+}
+
 int main(int argc, char* argv[]) {
 
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_Window* window = SDL_CreateWindow("title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,WINDOW_W,WINDOW_H,SDL_WINDOW_SHOWN);
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
-	SDL_RenderClear(renderer);
+    SDL_UpdateWindowSurface(window);
 
-	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 100);
+    SDL_RenderDrawLine(renderer, 0, 0, 500, 500);
+	//SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+    //DrawManyDiffEqLines(renderer);
 
 	//SDL_RenderDrawPoint(renderer, i, k);
     //DrawCircle(renderer, CENTER_W, CENTER_H, 30);
-    DrawManyDiffEqLines(renderer);
-
-
-    SDL_Surface* img = SDL_GetWindowSurface(window);
-    SDL_LockSurface(img);
-    SDL_SaveBMP(img, "out.bmp");
-    SDL_UnlockSurface(img);
+    /*SDL_Surface* img = SDL_GetWindowSurface(window);
+    SDL_SaveBMP(img, "out2.bmp");
+    SDL_UnlockSurface(img);*/
+    SaveScreenshotBMP(window, renderer);
 
     SDL_RenderPresent(renderer);
 	SDL_Delay(3000);
